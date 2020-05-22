a11253fe908b1fd95c3bb52dc765e0a99cc44e5d
18763552493@163.com <18763552493@163.com>
2020-05-22 17:29:23 +0800

    1.0.1代码提交 实现单张图片的压缩

--- app/src/main/java/com/zs/easy/imgcompress/demo/MainActivity.java
+++ app/src/main/java/com/zs/easy/imgcompress/demo/MainActivity.java
@@ -1,7 +1,15 @@
-package com.zs.easy.imgcompress;
+package com.zs.easy.imgcompress.demo;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.zs.easy.imgcompress.EasyImgCompress;
+import com.zs.easy.imgcompress.OnCompressMultiplePicsListener;
+import com.zs.easy.imgcompress.OnCompressSinglePicListener;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -9,13 +17,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+
         //场景一 把单张图片压缩到100k以内 同时像素不超过1200（宽、高都不大于1200）
-        EasyImgCompress.with(this).load("")
+        EasyImgCompress.withSinglePic(this, "/mnt/sdcard/ttt.jpg")
                 .maxPx(1200)
                 .maxSize(100)
                 .setOnCompressSinglePicListener(new OnCompressSinglePicListener() {
@@ -26,18 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onSuccess(File file) {
-
+                        Log.i("EasyImgCompress", "size = " + file.length() + " path= " + file.getAbsolutePath());
                     }
 
                     @Override
                     public void onError(String error) {
-
+                        Log.e("EasyImgCompress", "error = " + error);
                     }
                 }).start();
 
 
         //场景二 把多张图片每一张都压缩到100k以内 同时每张像素不超过1200（宽、高都不大于1200）
-        EasyImgCompress.with(this).load(new ArrayList<String>())
+        EasyImgCompress.withMultiPics(this, new ArrayList<String>())
                 .maxPx(1200)
                 .maxSize(100)
                 .setOnCompressMultiplePicsListener(new OnCompressMultiplePicsListener() {
@@ -47,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onSuccess(List<File> files) {
+                    public void onSuccess(List<File> successFiles) {
 
                     }
 
@@ -55,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onHasError(List<File> successFiles, List<String> errorImages, List<String> errorMsgs) {
 
                     }
-                }).start();
+                })
+                .start();
     }
 }
